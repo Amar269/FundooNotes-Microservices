a9433cfae099badcfeb60d142011edb4cc4fa29f
Amarnath Kolla <amarrama269@gmail.com>
2026-06-19 08:29:52 +0530

    Created IuserRepository  in the Userservcie Interfaces

---
+++ src/Services/UserService/UserService.Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UserService.Domain.Entities;
+
+namespace UserService.Application.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetUserByEmailAsync(string email);
+
+        Task<User?> GetUserByIdAsync(int id);
+
+        Task AddUserAsync(User user);
+
+        Task UpdateUserAsync(User user);
+
+        Task SaveChangesAsync();
+    }
+}
