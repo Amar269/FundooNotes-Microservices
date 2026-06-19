7e5696627e31dc0062d0d2fe4e303f8dd8d9a4b6
Amarnath Kolla <amarrama269@gmail.com>
2026-06-19 08:16:01 +0530

    Created User DTos in the Application layer of UserService

---
+++ src/Services/UserService/UserService.Application/DTOs/LoginDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserService.Application.DTOs
+{
+    public record LoginDto
+    (
+        string Email,
+        string Password
+    );
+}
---
+++ src/Services/UserService/UserService.Application/DTOs/RegisterUserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserService.Application.DTOs
+{
+    public record RegisterUserDto
+    (
+        string FirstName,
+        string LastName,
+        string Email,
+        string Password
+        );
+    
+}
