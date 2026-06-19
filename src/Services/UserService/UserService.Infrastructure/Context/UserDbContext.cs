173775b09e3b40815d1a28423cda2ae0229a2af3
Amarnath Kolla <amarrama269@gmail.com>
2026-06-19 08:37:20 +0530

    created the UserDbcontex in the UserInfrastructure layer

---
+++ src/Services/UserService/UserService.Infrastructure/Context/UserDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UserService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace UserService.Infrastructure.Context
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+
+}
