0e79c5d9b432751fa7e10aa4ba45a3e76c1d57cd
Amarnath Kolla <amarrama269@gmail.com>
2026-06-19 08:50:35 +0530

    Updated the code in the Dbcontext in UserService

---
+++ src/Services/UserService/UserService.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UserService.Application.Interfaces;
+using UserService.Domain.Entities;
+using UserService.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace UserService.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+
+        private readonly UserDbContext _context;
+
+        public UserRepository(UserDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddUserAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+        }
+
+        public async Task<User?> GetUserByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+
+        }
+
+        public async Task<User?> GetUserByIdAsync(int id)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.UserId == id);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public Task UpdateUserAsync(User user)
+        {
+            _context.Users.Update(user);
+
+            return Task.CompletedTask;
+        }
+
+    }
+}
