b7270a4500e7ae48c34bc8165d0c8eb554a097ba
Amarnath Kolla <amarrama269@gmail.com>
2026-06-20 11:33:36 +0530

    created the gobal exception files

---
+++ src/SharedLibrary/SharedLibrary.Exceptions/Exceptions/ConflictException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLibrary.Exceptions.Exceptions
+{
+    public class ConflictException : Exception
+    {
+        public ConflictException(string message) : base(message)
+        {
+
+        }
+
+    }
+}
---
+++ src/SharedLibrary/SharedLibrary.Exceptions/Exceptions/UnauthorizedOperationException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLibrary.Exceptions.Exceptions
+{
+    public class UnauthorizedOperationException : Exception
+    {
+        public UnauthorizedOperationException(string message): base(message)
+        {
+
+        }
+
+
+    }
+}
