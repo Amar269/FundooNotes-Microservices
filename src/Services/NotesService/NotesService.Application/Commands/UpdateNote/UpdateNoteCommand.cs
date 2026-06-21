1c40bad364cbf4b93f60f13c978e80a9e526c8de
Amarnath Kolla <amarrama269@gmail.com>
2026-06-21 21:38:11 +0530

    Added UpdateNote command structure

---
+++ src/Services/NotesService/NotesService.Application/Commands/UpdateNote/UpdateNoteCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using NotesService.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotesService.Application.Commands.UpdateNote
+{
+    public class UpdateNoteCommand : IRequest<NoteResponse?>
+    {
+        public long NoteId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Color { get; set; }
+    }
+}
