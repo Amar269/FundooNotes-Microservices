796fa402bdcb40d448c5395a552734e9d57df77c
Amarnath Kolla <amarrama269@gmail.com>
2026-06-21 23:21:02 +0530

    Added PinNote command structure

---
+++ src/Services/NotesService/NotesService.Application/Commands/PinNote/PinNoteCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotesService.Application.Commands.PinNote
+{
+    public class PinNoteCommand : IRequest<bool>
+    {
+        public long NoteId { get; set; }
+    }
+    
+}
---
+++ src/Services/NotesService/NotesService.Application/Commands/PinNote/PinNoteCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using NotesService.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotesService.Application.Commands.PinNote
+{
+    public class PinNoteCommandHandler :
+        IRequestHandler<PinNoteCommand, bool>
+    {
+        private readonly INoteRepository _noteRepository;
+
+        public PinNoteCommandHandler(
+            INoteRepository noteRepository)
+        {
+            _noteRepository = noteRepository;
+        }
+
+        public async Task<bool> Handle(
+            PinNoteCommand request,
+            CancellationToken cancellationToken)
+        {
+            return true;
+
+        }
+    }
+}
