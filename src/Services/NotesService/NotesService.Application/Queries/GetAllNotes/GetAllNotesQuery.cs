73860b91f354a7d22a4da0b99f7c1a4de8e89f5e
Amarnath Kolla <amarrama269@gmail.com>
2026-06-21 19:34:44 +0530

    Implemented GetAllNotes query and handler

---
+++ src/Services/NotesService/NotesService.Application/Queries/GetAllNotes/GetAllNotesQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using NotesService.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotesService.Application.Queries.GetAllNotes
+{
+    public class GetAllNotesQuery : IRequest<List<NoteResponse>>
+    {
+        public int UserId { get; set; }
+    }
+}
---
+++ src/Services/NotesService/NotesService.Application/Queries/GetAllNotes/GetAllNotesQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using NotesService.Application.DTOs;
+using NotesService.Application.Interfaces;
+using System.Linq;
+
+namespace NotesService.Application.Queries.GetAllNotes
+{
+    public class GetAllNotesQueryHandler :IRequestHandler<GetAllNotesQuery, List<NoteResponse>>
+    {
+        private readonly INoteRepository _noteRepository;
+
+        public GetAllNotesQueryHandler(INoteRepository noteRepository)
+        {
+            _noteRepository = noteRepository;
+        }
+
+        public async Task<List<NoteResponse>> Handle(
+            GetAllNotesQuery request,
+            CancellationToken cancellationToken)
+        {
+            var notes = await _noteRepository.GetAllNotesAsync(request.UserId);
+
+            return notes.Select(note => new NoteResponse
+            {
+                NoteId = note.NoteId,
+                Title = note.Title,
+                Description = note.Description,
+                Color = note.Color,
+                CreatedAt = note.CreatedAt,
+                IsPin = note.IsPin,
+                IsArchive = note.IsArchive,
+                IsTrash = note.IsTrash
+            }).ToList();
+        }
+    }
+    
+}
