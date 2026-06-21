ffb3b70c87c8414c026aa6ec53f0362bd0f006d8
Amarnath Kolla <amarrama269@gmail.com>
2026-06-21 22:20:13 +0530

    Added ArchiveNote command structure

---
+++ src/Services/NotesService/NotesService.Application/Commands/ArchiveNote/ArchiveNoteCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotesService.Application.Commands.ArchiveNote
+{
+    public class ArchiveNoteCommand : IRequest<bool>
+    {
+        public long NoteId { get; set; }
+    }
+    
+}
---
+++ src/Services/NotesService/NotesService.Application/Commands/ArchiveNote/ArchiveNoteCommandHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotesService.Application.Commands.ArchiveNote
+{
+    internal class ArchiveNoteCommandHandler
+    {
+    }
+}
