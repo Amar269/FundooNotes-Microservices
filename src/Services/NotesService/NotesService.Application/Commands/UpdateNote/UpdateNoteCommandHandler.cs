7f3603f5b1e501371f26ecb1e786812dfde27ae4
Amarnath Kolla <amarrama269@gmail.com>
2026-06-21 21:43:35 +0530

    Implemented UpdateNote command and handler

--- src/Services/NotesService/NotesService.Application/Commands/UpdateNote/UpdateNoteCommandHandler.cs
+++ src/Services/NotesService/NotesService.Application/Commands/UpdateNote/UpdateNoteCommandHandler.cs
@@ -1,10 +1,52 @@
-﻿using System;
+﻿using MediatR;
+using NotesService.Application.DTOs;
+using NotesService.Application.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace NotesService.Application.Commands.UpdateNote
 {
-    internal class UpdateNoteCommandHandler
+    public class UpdateNoteCommandHandler :
+       IRequestHandler<UpdateNoteCommand, NoteResponse?>
     {
+        private readonly INoteRepository _noteRepository;
+
+        public UpdateNoteCommandHandler(INoteRepository noteRepository)
+        {
+            _noteRepository = noteRepository;
+        }
+
+        public async Task<NoteResponse?> Handle(
+            UpdateNoteCommand request,
+            CancellationToken cancellationToken)
+        {
+            var note = await _noteRepository
+                .GetNoteByIdAsync(request.NoteId);
+
+            if (note == null)
+            {
+                return null;
+            }
+
+            note.Title = request.Title;
+            note.Description = request.Description;
+            note.Color = request.Color;
+            note.UpdatedAt = DateTime.UtcNow;
+
+            await _noteRepository.UpdateNoteAsync(note);
+
+            return new NoteResponse
+            {
+                NoteId = note.NoteId,
+                Title = note.Title,
+                Description = note.Description,
+                Color = note.Color,
+                IsPin = note.IsPin,
+                IsArchive = note.IsArchive,
+                IsTrash = note.IsTrash,
+                CreatedAt = note.CreatedAt
+            };
+        }
     }
 }
