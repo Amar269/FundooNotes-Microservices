aae37bb808abb94552ff14288140dda03ab85c15
Amarnath Kolla <amarrama269@gmail.com>
2026-06-21 22:47:39 +0530

    Added TrashNote command structure

---
+++ src/Services/NotesService/NotesService.Application/Commands/TrashNote/TrashNoteCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotesService.Application.Commands.TrashNote
+{
+     public class TrashNoteCommand : IRequest<bool>
+     {
+        public long NoteId { get; set; }
+     }
+
+}
---
+++ src/Services/NotesService/NotesService.Application/Commands/TrashNote/TrashNoteCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+using NotesService.Application.Interfaces;
+
+namespace NotesService.Application.Commands.TrashNote
+{
+    public class TrashNoteCommandHandler :
+        IRequestHandler<TrashNoteCommand, bool>
+    {
+        private readonly INoteRepository _noteRepository;
+
+        public TrashNoteCommandHandler(
+            INoteRepository noteRepository)
+        {
+            _noteRepository = noteRepository;
+        }
+
+        public async Task<bool> Handle( TrashNoteCommand request,CancellationToken cancellationToken)
+        {
+            return true;
+
+        }
+    }
+}
