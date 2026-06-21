c42e5329084fa5ff549671de61cc5129c16c2703
Amarnath Kolla <amarrama269@gmail.com>
2026-06-21 16:37:06 +0530

    configure notes service dependency injection

---
+++ src/Services/NotesService/NotesService.API/Controllers/NotesController.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using NotesService.Application.Commands.CreateNote;
+
+namespace NotesService.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NotesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public NotesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateNote(
+            CreateNoteCommand command)
+        {
+            var result =
+                await _mediator.Send(command);
+
+            return Ok(result);
+
+           
+        }
+    }
+}
