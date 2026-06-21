d4434fe425fd5d1dbff0d15e1d19aa8628fe1bf6
Amarnath Kolla <amarrama269@gmail.com>
2026-06-21 15:38:28 +0530

    add create note cqrs command structure

---
+++ src/Services/NotesService/NotesService.Application/Commands/CreateNote/CreateNoteCommand.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using NotesService.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotesService.Application.Commands.CreateNote
+{
+    public class CreateNoteCommand : IRequest<NoteResponse>
+    {
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string? Color { get; set; }
+
+        public DateTime? Reminder { get; set; }
+
+        public long UserId { get; set; }
+
+    }
+}
