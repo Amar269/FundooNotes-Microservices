33215c1f50ca7ed5a9e5fc76d8a06e952740d215
Amarnath Kolla <amarrama269@gmail.com>
2026-06-21 23:49:34 +0530

    Added ChangeColor command structure

---
+++ src/Services/NotesService/NotesService.Application/Commands/ChangeColor/ChangeColorCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotesService.Application.Commands.ChangeColor
+{
+    public class ChangeColorCommand : IRequest<bool>
+    {
+        public long NoteId { get; set; }
+
+        public string Color { get; set; }
+    }
+}
