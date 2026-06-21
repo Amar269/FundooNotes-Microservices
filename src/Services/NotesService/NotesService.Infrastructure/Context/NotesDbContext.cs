884b2582df905b821728f2b0dbd6811b52eedd89
Amarnath Kolla <amarrama269@gmail.com>
2026-06-21 15:00:04 +0530

    added  notes dbcontext

---
+++ src/Services/NotesService/NotesService.Infrastructure/Context/NotesDbContext.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotesService.Infrastructure.Context
+{
+    public class NotesDbContext : DbContext
+    {
+        public NotesDbContext(DbContextOptions<NotesDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Note> Notes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Note>()
+               .HasKey(x => x.NoteId);
+
+            modelBuilder.Entity<Note>()
+                .Property(x => x.Title)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            modelBuilder.Entity<Note>()
+                .Property(x => x.Description)
+                .HasMaxLength(2000);
+
+            modelBuilder.Entity<Note>()
+                .Property(x => x.IsPin)
+                .HasDefaultValue(false);
+
+            modelBuilder.Entity<Note>()
+                .Property(x => x.IsArchive)
+                .HasDefaultValue(false);
+
+            modelBuilder.Entity<Note>()
+                .Property(x => x.IsTrash)
+                .HasDefaultValue(false);
+
+            base.OnModelCreating(modelBuilder);
+            
+        }
+
+    }
+}
