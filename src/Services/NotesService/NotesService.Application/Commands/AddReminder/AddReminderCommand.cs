9c3eef04a71f1046b5ddb4fa9561b185327d52d4
Amarnath Kolla <amarrama269@gmail.com>
2026-06-22 00:05:30 +0530

    Added AddReminder command structure

---
+++ src/Services/NotesService/NotesService.Application/Commands/AddReminder/AddReminderCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+
+namespace NotesService.Application.Commands.AddReminder
+{
+    public class AddReminderCommand : IRequest<bool>
+    {
+        public long NoteId { get; set; }
+
+        public DateTime Reminder { get; set; }
+    }
+}
---
+++ src/Services/NotesService/NotesService.Application/Commands/AddReminder/AddReminderCommandHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotesService.Application.Commands.AddReminder
+{
+    internal class AddReminderCommandHandler
+    {
+    }
+}
