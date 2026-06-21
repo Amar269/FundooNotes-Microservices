25d546cf72fa3d2e9a772228154cfb4a1dbee347
Amarnath Kolla <amarrama269@gmail.com>
2026-06-21 22:00:23 +0530

    Added DeleteNote command structure

---
+++ src/Services/NotesService/NotesService.Application/Commands/DeleteNote/DeleteNoteCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+
+namespace NotesService.Application.Commands.DeleteNote
+{
+     public class DeleteNoteCommandHandler
+    {
+        public long NoteId { get; set; }
+    }
+}
