31c747e18fa6f5b5cc52e9ee9aa8d62f491ed297
Amarnath Kolla <amarrama269@gmail.com>
2026-06-22 00:24:40 +0530

    Implemented AddReminder command and handler

--- src/Services/NotesService/NotesService.Application/Commands/AddReminder/AddReminderCommandHandler.cs
+++ src/Services/NotesService/NotesService.Application/Commands/AddReminder/AddReminderCommandHandler.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MediatR;
+using NotesService.Application.Interfaces;
 
 namespace NotesService.Application.Commands.AddReminder
 {
-    internal class AddReminderCommandHandler
+    public class AddReminderCommandHandler : IRequestHandler<AddReminderCommand, bool>
     {
+        private readonly INoteRepository _noteRepository;
+
+        public AddReminderCommandHandler(INoteRepository noteRepository)
+        {
+            _noteRepository = noteRepository;
+        }
+
+        public async Task<bool> Handle(AddReminderCommand request,CancellationToken cancellationToken)
+        {
+            var note = await _noteRepository
+                .GetNoteByIdAsync(request.NoteId);
+
+            if (note == null)
+            {
+                return false;
+            }
+
+            note.Reminder = request.Reminder;
+
+            await _noteRepository.UpdateNoteAsync(note);
+
+            return true;
+        }
     }
 }
