02b560958f4a2dcba2c2df1ec2375ca432c9ad5c
Amarnath Kolla <amarrama269@gmail.com>
2026-06-22 11:39:23 +0530

    feat(user-service): add get user by email endpoint

---
+++ src/Services/UserService/UserService.Application/Queries/GetUserByEmail/GetUserByEmailQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UserService.Application.DTOs;
+
+namespace UserService.Application.Queries.GetUserByEmail
+{
+    internal class GetUserByEmailQuery : IRequest<UserDetailsDto>;
+    {
+    }
+}
