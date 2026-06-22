187a5a480e646c9b2c44cf3a7ffc1fcc0b5d4f19
Amarnath Kolla <amarrama269@gmail.com>
2026-06-22 10:19:16 +0530

    Added add collaborator command foundation

---
+++ src/Services/CollaboratorService/CollaboratorService.Application/Commands/AddCollaborator/AddCollaboratorCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using CollaboratorService.Application.DTOs;
+using MediatR;
+
+namespace CollaboratorService.Application.Commands.AddCollaborator
+{
+    public class AddCollaboratorCommand : IRequest<string>
+    {
+        public long OwnerUserId { get; set; }
+
+        public AddCollaboratorRequest Request { get; set; }
+            = new AddCollaboratorRequest();
+    }
+}
---
+++ src/Services/CollaboratorService/CollaboratorService.Application/Commands/AddCollaborator/AddCollaboratorCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+
+namespace CollaboratorService.Application.Commands.AddCollaborator
+{
+    public class AddCollaboratorCommandHandler :
+        IRequestHandler<AddCollaboratorCommand, string>
+    {
+        public async Task<string> Handle(
+            AddCollaboratorCommand request,
+            CancellationToken cancellationToken)
+        {
+            return "Collaborator Added";
+        }
+    }
+}
