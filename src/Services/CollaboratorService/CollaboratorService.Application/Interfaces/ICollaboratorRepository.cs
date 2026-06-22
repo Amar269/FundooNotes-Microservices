1dafc6bf0a7b2adc71d38839ec0ea0abdd6a5557
Amarnath Kolla <amarrama269@gmail.com>
2026-06-22 09:32:03 +0530

    Added collaborator repository interface

---
+++ src/Services/CollaboratorService/CollaboratorService.Application/Interfaces/ICollaboratorRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using CollaboratorService.Domain.Entities;
+
+namespace CollaboratorService.Application.Interfaces
+{
+    public interface ICollaboratorRepository
+    {
+        Task AddCollaboratorAsync(Collaborator collaborator);
+
+        Task RemoveCollaboratorAsync(long collaboratorId);
+
+        Task<IEnumerable<Collaborator>> GetCollaboratorsByNoteIdAsync(long noteId);
+
+        Task<IEnumerable<Collaborator>> GetSharedNotesAsync(long userId);
+
+        Task<Collaborator?> GetCollaboratorByIdAsync(long collaboratorId);
+
+        Task UpdateCollaboratorAsync(Collaborator collaborator);
+    }
+}
