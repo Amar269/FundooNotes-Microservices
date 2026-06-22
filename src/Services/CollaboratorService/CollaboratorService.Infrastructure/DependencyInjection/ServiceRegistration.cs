65ef159c51d4af9b90cff1b8bc20725dcacd6e96
Amarnath Kolla <amarrama269@gmail.com>
2026-06-22 09:58:09 +0530

    Configured collaborator service startup

---
+++ src/Services/CollaboratorService/CollaboratorService.Infrastructure/DependencyInjection/ServiceRegistration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CollaboratorService.Application.Interfaces;
+using CollaboratorService.Infrastructure.Context;
+using CollaboratorService.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CollaboratorService.Infrastructure.DependencyInjection
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<CollaboratorDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<ICollaboratorRepository, CollaboratorRepository>();
+
+            return services;
+        }
+    }
+}
