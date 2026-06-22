041e01aa0e89055bdef745ba38c283f210bfcf7e
Amarnath Kolla <amarrama269@gmail.com>
2026-06-22 09:18:00 +0530

    Added Collaborator DbContext

---
+++ src/Services/CollaboratorService/CollaboratorService.Infrastructure/Context/CollaboratorDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using CollaboratorService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CollaboratorService.Infrastructure.Context
+{
+    public class CollaboratorDbContext : DbContext
+    {
+        public CollaboratorDbContext(DbContextOptions<CollaboratorDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Collaborator> Collaborators { get; set; }
+    }
+}
