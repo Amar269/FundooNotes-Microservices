5fc995c4d9651b82fb2e30340a0c08c9b7aae211
Amarnath Kolla <amarrama269@gmail.com>
2026-06-23 08:27:37 +0530

    Implemented update collaborator permission endpoint

---
+++ src/Services/CollaboratorService/CollaboratorService.Application/Commands/UpdateCollaborator/UpdateCollaboratorCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CollaboratorService.Application.DTOs;
+using MediatR;
+
+namespace CollaboratorService.Application.Commands.UpdateCollaborator
+{
+    public class UpdateCollaboratorCommand : IRequest<string>
+    {
+        public long CollaboratorId { get; set; }
+
+        public UpdateCollaboratorDto Request { get; set; } = null!;
+    }
+}
---
+++ src/Services/CollaboratorService/CollaboratorService.Application/DTOs/UpdateCollaboratorDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollaboratorService.Application.DTOs
+{
+    public class UpdateCollaboratorDto
+    {
+        public string Permission { get; set; } = string.Empty;
+    }
+}
