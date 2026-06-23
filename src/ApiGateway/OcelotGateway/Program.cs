0888f61619f616010033d39bc4d5dac6109a6335
Amarnath Kolla <amarrama269@gmail.com>
2026-06-23 16:48:33 +0530

    Implemented CORS policies in API Gateway

--- src/ApiGateway/OcelotGateway/Program.cs
+++ src/ApiGateway/OcelotGateway/Program.cs
@@ -9,10 +9,36 @@ builder.Configuration
         optional: false,
         reloadOnChange: true);
 
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+             .AllowAnyHeader()
+             .AllowAnyMethod();
+
+    });
+    options.AddPolicy("RestrictedOrigin", policy =>
+    {
+        policy.WithOrigins("https://localhost:3000")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+
+});
+
 
 builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseCors("AllowAll");
+}
+else
+{
+    app.UseCors("RestrictedOrigin");
+}
 
 
 await app.UseOcelot();
