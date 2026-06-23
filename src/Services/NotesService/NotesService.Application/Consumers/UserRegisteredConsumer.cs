2100f7be16fd0b5a7085642130cfc14bc4b12961
Amarnath Kolla <amarrama269@gmail.com>
2026-06-24 02:11:31 +0530

    Added RabbitMQ consumer services

---
+++ src/Services/NotesService/NotesService.Application/Consumers/UserRegisteredConsumer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SharedLibrary.Contracts.Events;
+using NotesService.Application.Interfaces;
+using NotesService.Domain.Entities;
+
+namespace NotesService.Application.Consumers;
+
+public class UserRegisteredConsumer
+{
+    private readonly INoteRepository _noteRepository;
+
+    public UserRegisteredConsumer(INoteRepository noteRepository)
+    {
+        _noteRepository = noteRepository;
+    }
+
+    public async Task Consume(UserRegisteredEvent message)
+    {
+        var welcomeNote = new Note
+        {
+            UserId = message.UserId,
+
+            Title = "👋 Welcome to Fundoo Notes",
+
+            Description = "Welcome aboard! This note was created automatically for you. Start organizing your ideas, reminders and important tasks here. Happy Note Taking! 🚀",
+
+            Color = "Yellow",
+
+            Reminder = DateTime.UtcNow.AddDays(7),
+
+            IsPin = true,
+
+            IsArchive = false,
+
+            IsTrash = false
+        };
+
+        await _noteRepository.CreateNoteAsync(welcomeNote);
+    }
+}
---
+++ src/Services/NotesService/NotesService.Infrastructure/Messaging/RabbitMqConsumerService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text.Json;
+using SharedLibrary.Contracts.Events;
+using SharedLibrary.Messaging.Configuration;
+using NotesService.Application.Consumers;
+
+namespace NotesService.Infrastructure.Messaging
+{
+    public class RabbitMqConsumerService : BackgroundService
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+        private readonly RabbitMqSettings _settings;
+        public RabbitMqConsumerService(IServiceScopeFactory scopeFactory, IOptions<RabbitMqSettings> settings)
+        {
+            _scopeFactory = scopeFactory;
+            _settings = settings.Value;
+        }
+
+
+        protected override async Task ExecuteAsync( CancellationToken stoppingToken)
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
